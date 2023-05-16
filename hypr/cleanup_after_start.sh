8737882ffceb59d98efee2f8e6dea9775f93493d
bluebyt <bluebyt@gmail.com>
2023-05-16 18:25:00 -0400

    Add files via upload

---
+++ hypr/cleanup_after_start.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+sleep 5
+
+hyprctl keyword windowrule "workspace unset,thunar"
+hyprctl keyword windowrule "workspace unset,Alacritty"
+hyprctl keyword windowrule "workspace unset,gnome.Nautilus"
+
