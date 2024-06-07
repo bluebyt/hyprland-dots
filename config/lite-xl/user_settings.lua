b9130cf9f71a7fc40830783f792d35b85578210a
bluebyt <bluebyt@gmail.com>
2024-06-07 15:34:10 -0400

    Add files via upload

---
+++ config/lite-xl/user_settings.lua
@@ -0,0 +1,84 @@
+return {
+  ["config"] = {
+    ["animate_drag_scroll"] = true,
+    ["borderless"] = false,
+    ["code_font"] = {
+      ["fonts"] = {
+        [1] = {
+          ["name"] = "CaskaydiaCove Nerd Font Mono Regular",
+          ["path"] = "/home/bluebyt/.local/share/fonts/CascadiaCode/CaskaydiaCoveNerdFontMono-Regular.ttf"
+        },
+        [2] = {
+          ["name"] = "JetBrains Mono Regular",
+          ["path"] = "/usr/share/lite-xl/fonts/JetBrainsMono-Regular.ttf"
+        }
+      },
+      ["options"] = {
+        ["antialiasing"] = "subpixel",
+        ["bold"] = false,
+        ["hinting"] = "slight",
+        ["italic"] = false,
+        ["size"] = 15,
+        ["smoothing"] = false,
+        ["strikethrough"] = false,
+        ["underline"] = false
+      }
+    },
+    ["custom_keybindings"] = {},
+    ["disabled_plugins"] = {
+      ["autocomplete"] = true
+    },
+    ["enabled_plugins"] = {
+      ["linewrapping"] = true
+    },
+    ["font"] = {
+      ["fonts"] = {
+        [1] = {
+          ["name"] = "CaskaydiaCove Nerd Font Regular",
+          ["path"] = "/home/bluebyt/.local/share/fonts/CascadiaCode/CaskaydiaCoveNerdFont-Regular.ttf"
+        },
+        [2] = {
+          ["name"] = "Fira Sans Regular",
+          ["path"] = "/usr/share/lite-xl/fonts/FiraSans-Regular.ttf"
+        }
+      },
+      ["options"] = {
+        ["antialiasing"] = "subpixel",
+        ["bold"] = false,
+        ["hinting"] = "slight",
+        ["italic"] = false,
+        ["size"] = 15,
+        ["smoothing"] = false,
+        ["strikethrough"] = false,
+        ["underline"] = false
+      }
+    },
+    ["plugins"] = {
+      ["autowrap"] = {
+        ["enabled"] = true
+      },
+      ["linewrapping"] = {
+        ["enable_by_default"] = true,
+        ["guide"] = true,
+        ["indent"] = true,
+        ["mode"] = "word"
+      },
+      ["minimap"] = {
+        ["caret_color"] = {
+          [1] = 216,
+          [2] = 222,
+          [3] = 233,
+          [4] = 255
+        },
+        ["enabled"] = true,
+        ["selection_color"] = {
+          [1] = 216,
+          [2] = 222,
+          [3] = 233,
+          [4] = 102
+        }
+      }
+    },
+    ["theme"] = "tokyo-night"
+  }
+}
