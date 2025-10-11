18ab10a7a1236e86af3b24d8bd6df0fcb872bd16
bluebyt <bluebyt@gmail.com>
2025-10-11 18:00:01 -0400

    Update weather.sh

--- conky/grumimosa/scripts/weather.sh
+++ conky/grumimosa/scripts/weather.sh
@@ -5,9 +5,9 @@
 # so that conky will still display the weather when offline even though it doesn't up to date
 
 # you can use this or replace with yours
-api_key=60d7b980f7da638967fed7f0aaf80f84
+api_key=
 # get your city id at https://openweathermap.org/find and replace
-city_id=6050610
+city_id=
 
 url="api.openweathermap.org/data/2.5/weather?id=${city_id}&appid=${api_key}&cnt=5&units=metric&lang=en"
 curl ${url} -s -o ~/.cache/weather.json
